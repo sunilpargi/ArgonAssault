504c1b2f2c8618f27e8e7fb24c2fea47e5d229fa
sunilpargi <49692267+sunilpargi@users.noreply.github.com>
2020-02-27 00:33:24 +0530

    implemented music

---
+++ ArgonAssault/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    void Start()
+    {
+        Invoke("loadFirstScene", 1f);
+    }
+
+     void loadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
